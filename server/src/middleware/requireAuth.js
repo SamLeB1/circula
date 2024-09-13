dbe3d31408cf3d3a119377dd3cfdb7f4663d7792
Sam <leblanc.SAL1@gmail.com>
2024-09-13 09:34:31 -0300

    added auth middleware

---
+++ server/src/middleware/requireAuth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/User.js";
+
+export default async function (req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization)
+    return res.status(401).json({ msg: "Authorization token required." });
+
+  const token = authorization.split(" ")[1];
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+    req.user = await userModel.findById(_id);
+    next();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
