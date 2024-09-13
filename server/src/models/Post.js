e90abcd9e25ac2198782ffebfb33123bcf48a001
Sam <leblanc.SAL1@gmail.com>
2024-09-13 10:56:14 -0300

    added post model

---
+++ server/src/models/Post.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const postSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const postModel = mongoose.model("Post", postSchema);
+export default postModel;
