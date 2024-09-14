8e47709e0225aeb01598d964155574c1205380ff
Sam <leblanc.SAL1@gmail.com>
2024-09-14 19:11:14 -0300

    added comment model

---
+++ server/src/models/Comment.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const commentSchema = new Schema(
+  {
+    postId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const commentModel = mongoose.model("Comment", commentSchema);
+export default commentModel;
