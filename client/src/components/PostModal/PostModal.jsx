4a9e3fa0ae6fe3293eb94237d0ca40f84eb8fa86
Sam <leblanc.SAL1@gmail.com>
2024-09-18 18:07:30 -0300

    added top bar to post modal

--- client/src/components/PostModal/PostModal.jsx
+++ client/src/components/PostModal/PostModal.jsx
@@ -1,4 +1,5 @@
 import { createPortal } from "react-dom";
+import iconClose from "../../assets/images/icon-close.svg";
 import "./PostModal.css";
 
 export default function PostModal({ post, isOpen, setIsOpen }) {
@@ -7,7 +8,15 @@ export default function PostModal({ post, isOpen, setIsOpen }) {
     return createPortal(
       <>
         <div className="overlay" onClick={() => setIsOpen(false)} />
-        <div className="post-modal">{post.content}</div>
+        <div className="post-modal">
+          <div className="top-bar">
+            <div style={{ height: "36px", width: "36px" }} />
+            <h1>{post.firstName}'s Post</h1>
+            <button onClick={() => setIsOpen(false)} type="button">
+              <img src={iconClose} alt="Close" title="Close" />
+            </button>
+          </div>
+        </div>
       </>,
       document.getElementById("overlays")
     );
