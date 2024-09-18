f118149ec72d1c37be1c1315e34cef4cf06c885f
Sam <leblanc.SAL1@gmail.com>
2024-09-18 19:47:10 -0300

    added comment form to post modal

--- client/src/components/PostModal/PostModal.jsx
+++ client/src/components/PostModal/PostModal.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import { createPortal } from "react-dom";
+import axios from "axios";
+import useAuthContext from "../../hooks/useAuthContext.jsx";
 import iconClose from "../../assets/images/icon-close.svg";
 import "./PostModal.css";
 
 export default function PostModal({ post, isOpen, setIsOpen }) {
+  const [content, setContent] = useState("");
+  const { user } = useAuthContext();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post(
+        `${import.meta.env.VITE_SERVER}/comments`,
+        { postId: post._id, content },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => setContent(""))
+      .catch((err) => console.error(err));
+  }
+
   if (!isOpen) return null;
   else
     return createPortal(
@@ -16,6 +38,16 @@ export default function PostModal({ post, isOpen, setIsOpen }) {
               <img src={iconClose} alt="Close" title="Close" />
             </button>
           </div>
+          <form className="comment-form" onSubmit={handleSubmit}>
+            <textarea
+              id="content"
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Write a comment..."
+              rows="4"
+              value={content}
+            />
+            <input disabled={content === ""} type="submit" value="Post" />
+          </form>
         </div>
       </>,
       document.getElementById("overlays")
