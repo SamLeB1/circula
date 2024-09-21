287ea65764be1582cb9a7a38b34703abd1c9d0d4
Sam <leblanc.SAL1@gmail.com>
2024-09-21 16:01:57 -0300

    added delete button to post dropdown

--- client/src/components/PostDropdown/PostDropdown.jsx
+++ client/src/components/PostDropdown/PostDropdown.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import iconMore from "../../assets/images/icon-more.svg";
+import iconDelete from "../../assets/images/icon-delete.svg";
 import "./PostDropdown.css";
 
 export default function PostDropdown() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="post-dropdown">
-      <button className="btn-more" type="button" title="More">
+      <button
+        className="btn-more"
+        type="button"
+        title="More"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <img className="icon-more" src={iconMore} alt="More" />
       </button>
+      {isOpen && (
+        <div className="dropdown-list">
+          <button className="dropdown-btn" type="button">
+            <img className="dropdown-icon" src={iconDelete} alt="" />
+            <span>Delete</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
