827cf892d01c8b26467e4e39454b9b94ba561d3b
Sam <leblanc.SAL1@gmail.com>
2024-09-24 13:31:25 -0300

    added edit post form

--- client/src/components/EditPostModal/EditPostModal.jsx
+++ client/src/components/EditPostModal/EditPostModal.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { createPortal } from "react-dom";
 import "./EditPostModal.css";
 
 export default function EditPostModal({ post, setIsOpen }) {
+  const [content, setContent] = useState(post.content);
+
+  function handleSubmit() {
+    e.preventDefault();
+  }
+
   return createPortal(
     <>
       <div
@@ -9,7 +16,18 @@ export default function EditPostModal({ post, setIsOpen }) {
         style={{ zIndex: 11 }}
         onClick={() => setIsOpen(false)}
       />
-      <div className="edit-post-modal">{post.content}</div>
+      <div className="edit-post-modal">
+        <h1>Edit Post</h1>
+        <form className="edit-post-form" onSubmit={handleSubmit}>
+          <textarea
+            id="content"
+            onChange={(e) => setContent(e.target.value)}
+            rows="4"
+            value={content}
+          />
+          <input disabled={content === ""} type="submit" value="Save" />
+        </form>
+      </div>
     </>,
     document.getElementById("overlays")
   );
